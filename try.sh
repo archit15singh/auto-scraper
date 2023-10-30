d0f52e8644feb926ade7a201d94988621f86a1eb
Archit Singh <contact.architsingh@gmail.com>
2023-10-30 12:02:05 +0530

    new item

--- try.sh
+++ try.sh
@@ -4,3 +4,4 @@ python dynamic_scraper.py --start_url https://api.python.langchain.com/en/latest
 python dynamic_scraper.py --start_url https://huyenchip.com/ --domain huyenchip --max_depth 1
 python dynamic_scraper.py --start_url https://cookbook.openai.com/ --domain cookbook.openai.com --max_depth 1
 python dynamic_scraper.py --start_url https://www.latent.space/archive --domain www.latent.space --max_depth 1
+python dynamic_scraper.py --start_url https://aman.ai/ --domain aman.ai --max_depth 1
