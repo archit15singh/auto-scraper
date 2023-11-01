e79853c1ff9d1e4de6bc72031e28a46f743dd611
Archit Singh <contact.architsingh@gmail.com>
2023-11-02 01:02:35 +0530

    add llm call

---
+++ test.py
@@ -0,0 +1,29 @@
+from langchain.llms import Ollama
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} took {execution_time:.4f} seconds to execute.")
+        return result
+    return wrapper
+
+@timeit
+def run():
+    html_path = '/Users/architsingh/Documents/projects/auto-scraper/data/react.dev/https___react.dev_learn_state-a-components-memory.html'
+    with open(html_path, 'r') as f:
+        html_data = f.read()
+    
+    llm = Ollama(model="mistral-openorca")
+    query=f"extract the blog from this html in clean fashion: {html_data}"
+    print(len(query))
+    
+    res = llm.predict(query)
+    print(len(res))
+    print(res)
+
+if __name__ == "__main__":
+    run()
